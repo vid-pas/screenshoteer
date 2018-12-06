31807582482616f372e46f084433925f133bb1d5
vid <vid@paloalto.com>
2018-12-05 16:31:20 -0800

    replacing spaces, adding url and args to file name

--- index.js
+++ index.js
@@ -38,25 +38,38 @@ console.log(fullPage);
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     const d = new Date()
-    const height = !program.h?0:program.h;
+    const height = !program.h?0:program.h
     if (program.w && height) await page.setViewport({width: Number(program.w), height: Number(height)})
     if (program.emulate) await page.emulate(devices[program.emulate]);
     await page.goto(urlvalue)
     const title = await page.title()
-    const t = title.replace(/[/\\?%*:|"<>]/g, '-')
-    const args = process.argv.replace(/[/\\?%*:|"<>]/g, '-')
+    const t = title.replace(/[/\\?%*:|"<> ]/g, '-')
+    const arglist = new Array()
+    //set arglist
+    if (urlvalue) arglist.push(urlvalue.replace(/:\/\//g, '-'))
+    if (program.emulate) arglist.push(program.emulate)
+    if (program.w) arglist.push('w_' + program.w)
+    if (program.h) arglist.push('h_' + program.h)
+    if (program.pdf) arglist.push('pdf')
+    if (program.el) arglist.push('el_' + program.el)
+    if (program.fullpage) arglist.push('fullpage')
+    const args = arglist.join('-').replace(/[/\\?%*:|"<> ]/g, '-')
     if (program.waitfor) await page.waitFor(Number(program.waitfor))
+    var filename = t + "_" +  args  + "_" + d.getTime() + '.png'
     if (program.el) {
       const el = await page.$(program.el);
-      await el.screenshot({
-        path: `${t} ${args} ${program.el} ${d.getTime()}.png`
-      });
+      await el.screenshot({path: `${filename}`});
     } else {
-      await page.screenshot({path: t + " " +  args  + " " + d.getTime() + '.png', fullPage: fullPage})
+      await page.screenshot({path: filename, fullPage: fullPage})
     }
     await page.emulateMedia('screen')
-    if (program.pdf) await page.pdf({ path: t + " " +  args  + " " + d.getTime()  + '.pdf' })
-    console.log(t)
+    if (program.pdf) {
+      filename = t + "_" +  args  + "_" + d.getTime() + '.pdf'
+      await page.pdf({
+        path: filename
+      })
+    }
+    console.log(filename)
     await browser.close()
   }
 })()
